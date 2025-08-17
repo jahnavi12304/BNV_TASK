4adc2697f320ecdefe3924b0474e337bfe3644eb
jahnavi12304 <jahnavijain18@gmail.com>
2025-08-17 17:47:53 +0530

    Updated frontend code

--- frontend/src/utils/userService.jsx
+++ frontend/src/utils/userService.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const BASE_URL = "http://localhost:5000"
+export const BASE_URL = "https://bnv-task.onrender.com"
 const API_URL = `${BASE_URL}/api/users`
 
 const getUsers = async (page) => {
